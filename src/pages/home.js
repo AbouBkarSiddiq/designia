5c59212ed9deb50ba18139c309af0025b84fbaf5
Abou Bkar Siddiq <aboubkar786@gmail.com>
2021-01-28 13:02:03 +0500

    Completed!

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import '../App.css';
 import { useHistory } from 'react-router-dom'
-import Background1 from '../assets/Background1.png'
+import Background from '../assets/Background.png'
 import placeholder from '../assets/placeholder.png'
 import arrow from '../assets/arrow.svg'
 export default function Home() {
   let history = useHistory()
   return (
-    <div style={{ background: `transparent url(${Background1}) no-repeat scroll center`, backgroundSize: "" }} className='flex flex-1 items-center justify-center min-h-screen'>
+    <div style={{ background: `transparent url(${Background}) no-repeat scroll center`, backgroundSize: "" }} className='flex flex-1 items-center justify-center min-h-screen'>
       <div className='flex flex-col flex-1 md:flex-row justify-between items-center px-4 w-full md:py-12 md:px-24'>
         <div className='flex flex-col justify-end md:w-2/3 mt-4'>
           <h1 className='text-white font-sans font-normal md:text-4xl lg:text-6xl'>We make thing done!</h1>
